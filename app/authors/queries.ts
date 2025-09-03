a9c1e4a3280afc4d63418c20437d1b774b4549f3
Harshit Kumar <harshit.k@sprinto.com>
2025-09-04 00:07:57 +0530

    Fix

--- app/authors/queries.ts
+++ app/authors/queries.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_AUTHORS = gql`
   query GetAuthors($page: Int, $pageSize: Int, $filter: AuthorFilter) {
-    authors(page: $page, pageSize: $pageSize, $filter: $filter) {
+    authors(page: $page, pageSize: $pageSize, filter: $filter) {
       nodes {
         id
         name
